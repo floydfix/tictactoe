55b437176de9172d64e6bc12803bdc1c26128053
Roger Lemmon <roger@rogerlemmon.com>
2020-03-07 03:28:33 -0800

    adding tie (no one wins)

--- js/tic.js
+++ js/tic.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 		space.addClass(turn);
 
 		// check to see if game over
-		checkWin();
+		checkOver();
 
 		// change turns
 		if (turn == 'x')
@@ -24,26 +24,36 @@ $(document).ready(function(){
 	});
 
 	// function to check if game over
-	function checkWin() {
+	function checkOver() {
 		var s = [];
+		var filled = true;
 		// go over all of the spaces and push them into an array like
 		// ['x', 'o', 'x', 3, 4, 5, 6, 7, 8] (numbers are 'empty' squares)
 		var spaces = $('.space');
 		for (var i = 0; i < spaces.length; i++) {
 			var sp = $(spaces[i]);
-			s.push(sp.hasClass('x') ? 'x' : sp.hasClass('o') ? 'o' : i);
+			var whatsHere = sp.hasClass('x') ? 'x' : sp.hasClass('o') ? 'o' : i;
+			s.push(whatsHere);
+			if (whatsHere != 'x' && whatsHere != 'o')
+				filled = false;
 		}
 
 		// loop over all of the possible winning rows and if one matches goto win function
 		for (var t = 0; t < wins.length; t++) {
-			if(s[wins[t][0]] == s[wins[t][1]] && s[wins[t][1]] == s[wins[t][2]])
+			if(s[wins[t][0]] == s[wins[t][1]] && s[wins[t][1]] == s[wins[t][2]]) {
 				win(s[wins[t][0]]);
+				return false;
+			}
 		}
+
+		// if the board is filled with letters and you got here, it is a tie
+		if (filled)
+			win(0);
 	}
 	
 	// simply console.log at this point
 	// TODO: add points! Scoreboard
 	function win(winner) {
-		console.log( winner + " won!");
+		console.log(winner != 0 ? (winner + " won!") : "tie");
 	}
 });
